2b3c19f084d81cb3f1f3d332623d53b9054c16f8
Mitoed <masaulls@gmail.com>
2023-10-03 21:59:30 +0300

    feat: new user verfication added

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 
 document.getElementById('login_error').innerHTML = ''
 document.getElementById('create_error').innerHTML = ''
+// RegEx that checks for 1 letter, 1 number, and 8 characters long
+const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d).{8,}$/
 
 
 
@@ -106,29 +108,25 @@ function DBInfoExist (database, checkField, checkValue, errorID = '') {
 // ---------- LOGIN PAGE ---------- //
 
 
-// Verify password of a given username
-function UserPassCorrect (database, checkUsername, checkPassword, errorID) {
-  if (database === null || checkUsername === null || checkPassword === null || errorID === null) {
-    console.log('Please provide all the inputs.')
-    return
-  }
+// Verify login credentials
+function UserPassCorrect (database, checkUsername, checkPassword) {
   for (let entry in database) {
     if (database[entry]['username'] === checkUsername) {
       if (database[entry]['password'] === checkPassword) {
-        document.getElementById(errorID).innerHTML = ''
+        document.getElementById('login_error').innerHTML = ''
         window.location.href = './enter_session.html'
         return
       } else {
-        document.getElementById(errorID).innerHTML = 'incorrect password'
+        document.getElementById('login_error').innerHTML = 'incorrect password'
         return
       }
     }
   }
-  document.getElementById(errorID).innerHTML = 'username does not exist'
+  document.getElementById('login_error').innerHTML = 'username does not exist'
   return
 }
 
-// When user clicks "login", validate the username and password then go to the Enter Session page.
+// Add user verification to button
 const login_exist_button = document.getElementById('login_exist')
 
 login_exist_button.onclick = function(event) {
@@ -136,14 +134,43 @@ login_exist_button.onclick = function(event) {
 
   let login_user = document.getElementById('username_input').value
   let password_input = document.getElementById('password_input').value
-  UserPassCorrect(databaseUSERS, login_user, password_input, 'login_error')
+  UserPassCorrect(databaseUSERS, login_user, password_input)
 }
 
-/* Login Page (Create User):
-- check to see if username exists in DB
-- check to see if password fits requirements
-- if passwords does not fit requirements or passwords don't match, show error message.
-*/
+// Verify new user credentials
+function UserPassCreate (database, newUsername, newPassword, confirmPassword) {
+  for (let entry in database) {
+    if (database[entry]['username'] === newUsername) {
+      document.getElementById('create_error').innerHTML = newUsername + ' already exists'
+      return
+    }
+  }
+  if (passwordRegex.test(newPassword)) {} else {
+    document.getElementById('create_error').innerHTML = 'password must contain 1 letter, 1 number, and be at least 8 characters long'
+    return
+  }
+  if (newPassword === confirmPassword) {} else {
+    document.getElementById('create_error').innerHTML = 'passwords must match'
+    return
+  }
+  window.location.href = './enter_session.html'
+  return
+}
+
+// Add new user verification to button
+const login_create_button = document.getElementById('login_create')
+
+login_create_button.onclick = function(event) {
+  event.preventDefault();
+
+  let new_username = document.getElementById('new_username').value
+  let new_password = document.getElementById('new_password').value
+  let confirm_password = document.getElementById('confirm_password').value
+  UserPassCreate(databaseUSERS, new_username, new_password, confirm_password)
+}
+
+
+
 
 /* Enter Session Page (Existing Session):
 - enter username into document.getElementById('username').innerHTML

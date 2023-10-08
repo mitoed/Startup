7d4c955e9b43b55f5d96202349d1475a93b0b2ed
Mitoed <masaulls@gmail.com>
2023-10-08 15:54:27 +0300

    Cat. inside sessionID + voting page error handle

--- main.js
+++ main.js
@@ -4,6 +4,9 @@
 
 const databaseUSERS = []
 const databaseSESSION = []
+const foodIDLetters = 'BCDFGHJ'
+const movieIDLetters = 'KLMNPQ'
+const gameIDLetters = 'RSTVWXZ'
 let currentUser
 let currentSessionID
 let currentSessionInstance
@@ -96,19 +99,18 @@ function addFakeUser(fakeUserName, fakePassword, fakeSessionID, fakeSelection) {
   let fakeUser = new User(fakeUserName, fakePassword, fakeSessionID, fakeSelection)
   databaseUSERS.push(fakeUser)
 }
-addFakeUser("masaulls", "1234", 'M7G2X7', 'Bumblebees')
-addFakeUser('chsaulls', '389d9*', 'M7G2X7', 'Costa Vida')
-addFakeUser('rcsaulls', '303udsd', 'M7G2X7', 'Five Sushi Bros')
-addFakeUser('ecsaulls', '38&jdkf', 'M7G2X7', 'Brick Oven')
-addFakeUser('ssaulls', '7329fd', 'M7G2X7', 'Burger Supreme')
-addFakeUser('csaulls', '39fds', 'M7G2X7', 'Good Move')
+addFakeUser("masaulls", "1234", 'F7G2X7', 'Bumblebees')
+addFakeUser('chsaulls', '389d9*', 'F7G2X7', 'Costa Vida')
+addFakeUser('rcsaulls', '303udsd', 'F7G2X7', 'Five Sushi Bros')
+addFakeUser('ecsaulls', '38&jdkf', 'F7G2X7', 'Brick Oven')
+addFakeUser('ssaulls', '7329fd', 'F7G2X7', 'Burger Supreme')
+addFakeUser('csaulls', '39fds', 'F7G2X7', 'Good Move')
 
 function addFakeSession(fakeSessionID, fakeCategory, databaseCATEGORY) {
   let fakeSession = new Session(fakeSessionID, fakeCategory, databaseCATEGORY, Date.now())
   databaseSESSION.push(fakeSession)
 }
 
-{ // Dummy databases
   let listFOOD = [
   "Wendy's",
   "McDonald's",
@@ -152,30 +154,28 @@ let listGAME = [
   "Agricola",
   "Scythe",
   "Splendor",
-  "Betrayal at Baldur's Gate"
-];
+  "Betrayal at Baldur's Gate"]
 
-addFakeSession ('M7G2X7', 'food', listFOOD)
-addFakeSession ("M7N7V4", 'food', listFOOD)
-addFakeSession ("S7T4H9", 'food', listFOOD)
+addFakeSession ('J5P6D9', 'food', listFOOD)
+addFakeSession ("F7N7V4", 'food', listFOOD)
+addFakeSession ("C7T4H9", 'food', listFOOD)
 addFakeSession ("F2W7Q7", 'food', listFOOD)
 addFakeSession ("H6Q2N0", 'food', listFOOD)
-addFakeSession ("S2S4D6", 'food', listFOOD)
-addFakeSession ("C7M0X2", 'game', listGAME)
+addFakeSession ("D2S4D6", 'food', listFOOD)
+addFakeSession ("K4X6J2", 'movie', listMOVIE)
+addFakeSession ("N7T5Q6", 'movie', listMOVIE)
+addFakeSession ("L3V9B4", 'movie', listMOVIE)
+addFakeSession ("N2K6M6", 'movie', listMOVIE)
+addFakeSession ("P7X5C7", 'movie', listMOVIE)
+addFakeSession ("K5F2K4", 'movie', listMOVIE)
+addFakeSession ("L2K3N3", 'movie', listMOVIE)
+addFakeSession ("R7M0X2", 'game', listGAME)
 addFakeSession ("X7H3J3", 'game', listGAME)
-addFakeSession ("N4C3Q5", 'game', listGAME)
-addFakeSession ("D5H9K5", 'game', listGAME)
+addFakeSession ("S4C3Q5", 'game', listGAME)
+addFakeSession ("T5H9K5", 'game', listGAME)
 addFakeSession ("S2V2J9", 'game', listGAME)
 addFakeSession ("Z6L3X3", 'game', listGAME)
 addFakeSession ("T7F3P7", 'game', listGAME)
-addFakeSession ("H4X6J2", 'movie', listMOVIE)
-addFakeSession ("N7T5Q6", 'movie', listMOVIE)
-addFakeSession ("F3V9B4", 'movie', listMOVIE)
-addFakeSession ("N2K6M6", 'movie', listMOVIE)
-addFakeSession ("Z7X5C7", 'movie', listMOVIE)
-addFakeSession ("Q5F2K4", 'movie', listMOVIE)
-addFakeSession ("V2K3N3", 'movie', listMOVIE)
-}
 
 // =============================================================================
 // GENERAL FUNCTIONS
@@ -361,7 +361,7 @@ function createSessionWithCategory() {
         break
     }
 
-    let newSessionInstance = new Session(createSessionID(), sessionCategory, listCATEGORY, Date.now())
+    let newSessionInstance = new Session(createSessionID(sessionCategory), sessionCategory, listCATEGORY, Date.now())
     databaseSESSION.push(newSessionInstance)
     let newSessionID = newSessionInstance['sessionID']
     window.location.href = `./voting_session.html?user=${currentUser}&session=${newSessionID}`
@@ -369,45 +369,74 @@ function createSessionWithCategory() {
   }
 }
 
-function createSessionID() {
+function createSessionID(sessionCategory) {
   let newSessionID
   let problem = false
   do {
-    newSessionID = randomSessionID()
+    newSessionID = randomSessionID(sessionCategory)
     problem = validateSessionID(newSessionID)
   } while (problem)
   return newSessionID
 }
 
-function randomSessionID() {
+function randomSessionID(sessionCategory) {
   let sessionString = []
-  let digit
-  let digitArray1 = 'BCDFGHJKLMNPQRSTVWXZ'.split('')
-  let digitArray2 = '23456789'.split('')
-  do {
-    digit = digitArray1[Math.floor(Math.random() * digitArray1.length)]
-    sessionString.push(digit)
-    digit = digitArray2[Math.floor(Math.random() * digitArray2.length)]
-    sessionString.push(digit)
-  } while (sessionString.length < 6)
+  let digitArray1
+  switch (true) {
+    case sessionCategory === 'food':
+      digitArray1 = foodIDLetters.split('')
+      break
+    case sessionCategory === 'movie':
+      digitArray1 = movieIDLetters.split('')
+      break
+    case sessionCategory === 'game':
+      digitArray1 = gameIDLetters.split('')
+      break
+  }
+  let digitArray2 = 'BCDFGHJKLMNPQRSTVWXZ'.split('')
+  let digitArray3 = '23456789'.split('')
+
+  sessionString.push(randomDigit(digitArray1))
+  sessionString.push(randomDigit(digitArray3))
+  sessionString.push(randomDigit(digitArray2))
+  sessionString.push(randomDigit(digitArray3))
+  sessionString.push(randomDigit(digitArray2))
+  sessionString.push(randomDigit(digitArray3))
+
   sessionString = sessionString.join('')
+  console.log(sessionString)
   return sessionString
+}
+
+function randomDigit(digitArray) {
+  return digitArray[Math.floor(Math.random() * digitArray.length)]
 }
 
 // =============================================================================
 // VOTING SESSION PAGE FUNCTIONALITY
 // =============================================================================
 
-function loadVotingSessionPage(defaultSessionID) {
+function loadVotingSessionPage() {
     currentSessionInstance = databaseSESSION.find((element) => element['sessionID'] === currentSessionID)
   if (currentSessionInstance === undefined) {
-    // if session is missing from the database; revert to default
-    document.getElementById('session_id').innerHTML = `Session ID: ${defaultSessionID}`;
-    currentSessionInstance = databaseSESSION.find((element) => element['sessionID'] === defaultSessionID)
-    console.log('Error: session not correctly loaded from database. Default inert session loaded instead.')
+    // if session is missing from the database; create new session but log error
+    switch (true) {
+      case foodIDLetters.includes(currentSessionID[0]):
+        currentCategory = 'food'
+        break
+      case movieIDLetters.includes(currentSessionID[0]):
+        currentCategory = 'movie'
+        break
+      case gameIDLetters.includes(currentSessionID[0]):
+        currentCategory = 'game'
+        break
+    }
+    currentSessionInstance = new Session(currentSessionID, 'food', listFOOD, Date.now())
+    console.log('Error: Session not correctly loaded from database; new session added instead. ' + 
+      'Please verify code for database entry creation and retrieval.')
   }
   document.title = `Voting Session: ${currentSessionID}`
-  
+
   populateTable(currentSessionInstance.category, currentSessionInstance.categoryArray)
 }
 
@@ -594,7 +623,7 @@ switch (true) {
   case window.location.href.includes('voting_session.html'):
     infoFromURL()
     infoToMenu()
-    loadVotingSessionPage('M7G2X7')
+    loadVotingSessionPage()
     castVoteButton()
     break
   case window.location.href.includes('about.html'):

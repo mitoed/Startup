99f108da416afac6c85c71c20f9814fde6b62abd
Mitoed <masaulls@gmail.com>
2023-10-26 20:23:25 +0300

    fix: remove double code in main.js

--- public/main.js
+++ public/main.js
@@ -974,17 +974,3 @@ function endSession() {
         }
     }
 }
-
-const yelp = require('yelp-fusion');
-const client = yelp.client(process.env.YELP_API_KEY);
-
-client.search({
-    term: 'Four Barrel Coffee',
-    location: 'san francisco, ca',
-})
-.then(response => {
-    console.log(response.jsonBody.businesses[0].name);
-})
-.catch(e => {
-    console.log(e);
-});

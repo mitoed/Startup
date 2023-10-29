383e42849cc785c79414c1b00330b50679c3861f
Mitoed <masaulls@gmail.com>
2023-10-29 17:59:17 +0300

    try the node.js search function example

--- index.js
+++ index.js
@@ -15,28 +15,21 @@ app.use(express.json());
 // Use CORS
 app.use(cors())
 
-const client = yelp.client(apiKey)
+// Node.js search example function
+const sdk = require('api')('@yelp-developers/v1.0#420s3alobgub91');
 
 // Route for fetching Yelp data and returning it as JSON
 app.get('/get-yelp-data', async (req, res) => {
-    // Axios request to Yelp API
-    const options = {
-        headers: {
-            accept: 'application/json',
-            Authorization: `Bearer ${apiKey}`
-        }
-    };
-
-    axios.get('https://api.yelp.com/v3/businesses/search?location=provo%2C%20ut&term=restaurant&open_now=true&sort_by=best_match&limit=10', options)
-        .then(response => {
-            // Data is available in response.data
-            console.log(response.data);
-            res.json(response.data);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({ error: 'Failed to fetch Yelp data' });
-        });
+    sdk.auth(`Bearer ${apiKey}`);
+    sdk.v3_business_search({
+        location: 'provo%2C%20ut',
+        term: 'restaurant',
+        open_now: 'true',
+        sort_by: 'best_match',
+        limit: '10'
+    })
+    .then(({ data }) => console.log(data))
+    .catch(err => console.error(err));
 });
 
 // Serve up the frontend static content hosting

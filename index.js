bb89e061c60c35bc9569506a68d7495b4e9ed57f
Mitoed <masaulls@gmail.com>
2023-10-29 17:49:20 +0300

    use axios instead of node-fetch

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors')
-const fetch = require('node-fetch')
+const axios = require('axios')
 const yelp = require('yelp-fusion')
 const apiKey = process.env.YELP_API_KEY
 
@@ -28,28 +28,10 @@ app.get('/get-yelp-data', async (req, res) => {
         }
       };
       
-      fetch('https://api.yelp.com/v3/businesses/search?location=provo%2C%20ut&term=restaurant&open_now=true&sort_by=best_match&limit=10', options)
+    axios.get('https://api.yelp.com/v3/businesses/search?location=provo%2C%20ut&term=restaurant&open_now=true&sort_by=best_match&limit=10', options)
         .then(response => response.json())
         .then(response => console.log(response))
         .catch(err => console.error(err));
-
-    // GitHub function
-    /*try {
-        // Fetch data from Yelp Fusion API
-        const response = await client.search({
-            term: 'Four Barrel Coffee',
-            location: 'san francisco, ca',
-        });
-
-        // Extract the name of the first business
-        console.log(response)
-        const businessName = response.jsonBody.businesses[0].name;
-
-        res.json({ name: businessName });
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ error: 'Failed to fetch Yelp data' });
-    }*/
 });
 
 // Serve up the frontend static content hosting

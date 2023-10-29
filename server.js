50f64dfeb38a165059b809cb689f1a1971e023e1
Mitoed <masaulls@gmail.com>
2023-10-29 20:59:50 +0300

    testing modules and framework code

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+// The service port. In production the frontend code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
+// Serve up the frontend static content hosting
+app.use(express.static('public'));
+
+app.get('/store/:storeName', (req, res) => {
+    res.send({name: req.params.storeName})
+})
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+module.exports = app

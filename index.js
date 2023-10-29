8a2f11fa9248dbdb21a93ca2401596497eeeb979
Mitoed <masaulls@gmail.com>
2023-10-29 17:56:50 +0300

    recopy code from chatgpt

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors')
-const axios = require('axios')
+const axios = require('axios'); // Import Axios
 const yelp = require('yelp-fusion')
 const apiKey = process.env.YELP_API_KEY
 
-// The service port. In production the frontend code is statically hosted by the service on the same port.
+// The service port. In production, the frontend code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 // JSON body parsing using built-in middleware
@@ -19,42 +19,24 @@ const client = yelp.client(apiKey)
 
 // Route for fetching Yelp data and returning it as JSON
 app.get('/get-yelp-data', async (req, res) => {
-    // Yelp Search Example Function
+    // Axios request to Yelp API
     const options = {
         headers: {
-          accept: 'application/json',
-          Authorization: `Bearer ${apiKey}`
+            accept: 'application/json',
+            Authorization: `Bearer ${apiKey}`
         }
-      };
-      
-      axios.get('https://api.yelp.com/v3/businesses/search?location=provo%2C%20ut&term=restaurant&open_now=true&sort_by=best_match&limit=10', options)
-      .then(response => {
-          // Data is available in response.data
-          console.log(response.data);
-          res.json(response.data);
-      })
-      .catch(err => {
-          console.error(err);
-          res.status(500).json({ error: 'Failed to fetch Yelp data' });
-      });
+    };
 
-    // GitHub function
-    /*try {
-        // Fetch data from Yelp Fusion API
-        const response = await client.search({
-            term: 'Four Barrel Coffee',
-            location: 'san francisco, ca',
+    axios.get('https://api.yelp.com/v3/businesses/search?location=provo%2C%20ut&term=restaurant&open_now=true&sort_by=best_match&limit=10', options)
+        .then(response => {
+            // Data is available in response.data
+            console.log(response.data);
+            res.json(response.data);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: 'Failed to fetch Yelp data' });
         });
-
-        // Extract the name of the first business
-        console.log(response)
-        const businessName = response.jsonBody.businesses[0].name;
-
-        res.json({ name: businessName });
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ error: 'Failed to fetch Yelp data' });
-    }*/
 });
 
 // Serve up the frontend static content hosting

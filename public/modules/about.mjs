cdcfc797a593edc89c1bb34c1c7529f4a6d92f54
Mitoed <masaulls@gmail.com>
2023-11-08 14:52:42 +0300

    move api to frontend

--- public/modules/about.mjs
+++ public/modules/about.mjs
@@ -14,12 +14,12 @@ async function insertQuote() {
 
 // 4.1 ---- Get quote and citation
 // 4.1.1 -- Send request through "Quotable" api
-        const response = await fetch('/api/get-quote')
+        const response = await fetch('https://api.quotable.io/random')
         const data = await response.json()
 
 // 4.1.2 -- Parse response
-        quote = data.quote
-        citation = '- ' + data.citation
+        quote = data.content
+        citation = '- ' + data.author
 
 // 4.2 ---- Insert quote into page
 // 4.2.1 -- If error in loading quote from api, use generic quote

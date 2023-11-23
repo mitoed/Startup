db3db8a4b5bd6a98fd86ebc35d4e4e6258b79ad4
Mitoed <masaulls@gmail.com>
2023-11-23 22:37:55 +0300

    add main.js to app render

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 import { Login } from './login/login.jsx'
-import { Enter } from './enter/enter_session.jsx'
+import { Enter } from './enter/enter.jsx'
 import { Voting } from './voting/voting_session.jsx'
 import { About } from './about/about.jsx'
 import './app.css'
@@ -9,35 +9,38 @@ import './app.css'
 export default function App() {
 
     return (
-        <BrowserRouter>
-            <div>
-                <header className="ALL-l-header">
-                    <section id="logo">
-                        <img src="/orange cowboy hat.png" alt="Orange Cowboy Hat Logo" width="100px" />
-                            <h1>Activity Anarchy</h1>
-                    </section>
-                    <nav>
-                        <menu className="ALL-menu" id="navigation_menu">
-                            <NavLink to='/'>Login</NavLink>
-                            <NavLink to='/enter'>Enter Session</NavLink>
-                            <NavLink to='/about'>About</NavLink>
-                        </menu>
-                    </nav>
-                </header>
-                <Routes>
-                    <Route path='/' element={<Login />} exact/>
-                    <Route path='/enter' element={<Enter />} exact/>
-                    <Route path='/voting' element={<Voting />} exact/>
-                    <Route path='/about' element={<About />} exact/>
-                    <Route path='*' element={<NotFound />}/>
-                </Routes>
-                <footer className="ALL-l-footer">
-                    <p><span className="Author-Name">Michael Saulls</span> | <a
-                        href="https://github.com/mitoed/Startup.git">GitHub
-                        Repository</a></p>
-                </footer>
-            </div>
-        </BrowserRouter>
+        <>
+            <BrowserRouter>
+                <div>
+                    <header className="ALL-l-header">
+                        <section id="logo">
+                            <img src="/orange cowboy hat.png" alt="Orange Cowboy Hat Logo" width="100px" />
+                                <h1>Activity Anarchy</h1>
+                        </section>
+                        <nav>
+                            <menu className="ALL-menu" id="navigation_menu">
+                                <NavLink to='/'>Login</NavLink>
+                                <NavLink to='/enter'>Enter Session</NavLink>
+                                <NavLink to='/about'>About</NavLink>
+                            </menu>
+                        </nav>
+                    </header>
+                    <Routes>
+                        <Route path='/' element={<Login />} exact/>
+                        <Route path='/enter' element={<Enter />} exact/>
+                        <Route path='/voting' element={<Voting />} exact/>
+                        <Route path='/about' element={<About />} exact/>
+                        <Route path='*' element={<NotFound />}/>
+                    </Routes>
+                    <footer className="ALL-l-footer">
+                        <p><span className="Author-Name">Michael Saulls</span> | <a
+                            href="https://github.com/mitoed/Startup.git">GitHub
+                            Repository</a></p>
+                    </footer>
+                </div>
+            </BrowserRouter>
+            <script src="main.js"></script>
+        </>
     )
 }
 
